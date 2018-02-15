5472bfca73d28dc9ca2c98d1a484cf3e3e6ebf17
vantonytran <vantonytran97@gmail.com>
2018-02-15 14:59:26 +0100

    added Button to calculate the Fibonacci number with a n-number from the slider

---
+++ Phoneword/Phoneword/FibbonacciPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public partial class FibbonacciPage : ContentPage
+    {
+        private double stepValue;
+        private Slider SliderMain;
+        private Label number;
+        private Label fibo;
+        public FibbonacciPage()
+        {
+            stepValue = 1.0;
+
+            SliderMain = new Slider
+            {
+                Minimum = 0.0f,
+                Maximum = 12.0f,
+                Value = 0.0f,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+            SliderMain.ValueChanged += OnSliderValueChanged;
+            number = new Label();
+            fibo = new Label();
+
+            Content = new StackLayout
+            {
+                Children = { SliderMain, number, fibo },
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / stepValue);
+
+            SliderMain.Value = newStep * stepValue;
+            number.Text = SliderMain.Value.ToString();
+            fibo.Text = Fibonumber(SliderMain.Value).ToString();
+        }
+        private double Fibonumber(double n)
+        {
+            if(n<=1.0){
+                return n;
+            }
+            if(n>=12.0){
+                return 100;
+            }
+            return (Fibonumber(n - 1)) + (Fibonumber(n - 2));
+        }
+    }
+}
---
+++ Phoneword/Phoneword/Fibo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Phoneword
+{
+    public class Fibo : ContentPage
+    {
+        private double stepValue;
+        public Fibo()
+        {
+            stepValue = 1.0;
+
+            SliderMain = new Slider
+            {
+                Minimum = 0.0f,
+                Maximum = 5.0f,
+                Value = 0.0f,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
+
+            SliderMain.ValueChanged += OnSliderValueChanged;
+
+            Content = new StackLayout
+            {
+                Children = { SliderMain },
+                Orientation = StackOrientation.Vertical,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+        }
+
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var newStep = Math.Round(e.NewValue / StepValue);
+
+            SliderMain.Value = newStep * StepValue;
+        }
+        }
+    }
+}
