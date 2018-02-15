71b4a7f77494552484f92239621388c9db2dcc17
vantonytran <vantonytran97@gmail.com>
2018-02-15 10:08:30 +0100

    visual studio project setup

---
+++ test_xamarin/test_xamarin/test_xamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace test_xamarin
+{
+    public partial class test_xamarinPage : ContentPage
+    {
+        public test_xamarinPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
